1715c410ff9f8353b8bc212fec48e760c84415b6
naisannovel <naisannovel@gmail.com>
2021-05-28 19:16:50 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './component/Home/Home';
 import Login from './component/Login/Login';
 import Signup from './component/Signup/Signup';
 
 const App = () => {
   return (
     <div>
+      <Home/>
       <Login/>
       <Signup/>
     </div>

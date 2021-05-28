f627f95154e3f756676e3a27f718f7e195ab90e4
Sarwar-Putul <sarwarputul@gmail.com>
2021-05-28 22:46:23 +0600

    update

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>this is Login page</h1>
+            <h1>Team no 61</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, ab?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, sapiente?</p>
         </div>
     );
 };
